6ee5a2075cb7345d72c72f32f56f19b94e552679
Diego <diego.vid.eco@gmail.com>
2018-01-11 23:34:28 -0600

    readme

---
+++ how-to.js
@@ -0,0 +1,15 @@
+console.log(`
+===================
+
+\`nodemon how-to.js\`
+
+===================
+
+/**
+ Se trata del script
+  "scripts": {
+    "watch": "nodemon"
+  }
+**/
+`)
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Cómo Watchear
+
+correr con npm run watcher how-to.js
+
+## Se trata del script
+```  
+"scripts": {
+  "monitor": "nodemon"
+}
+```
